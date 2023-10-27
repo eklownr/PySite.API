c86d726a845bab91f905d350fa935c819859e59d
eklownr <eklownr@gmail.com>
2023-10-28 00:23:55 +0200

    def response_to_dict

--- flask_app/api_helper.py
+++ flask_app/api_helper.py
@@ -8,23 +8,17 @@ def get_response(target_url="http://jsonplaceholder.typicode.com/users"):
 def response_to_list(response):
     i = 0 
     name_list = []
-    for  my_dict in response:
+    for person_dict in response:
         name_list.append(response[i]["name"])
         i += 1
     return name_list
 
 def response_to_dict(response, dict_key="name"):
-    i = 0 
-    user_dict = []
-    for  all_dict in response:
-        user_dict.append(response[i][dict_key])
-        i += 1
+    ''' return dict{ id: name } '''
+    user_dict = {}
+    for all_dict in response:
+        user_dict[all_dict["id"]] = all_dict[dict_key]
     return user_dict
 
-# def name_from_list(name_list):
-#     i = 0
-#     for name in name_list:
-#         print(f"index: {i} namn: {name}")
-#         i += 1
 # TEST
-# name_from_list(response_to_list(get_response()))
+# print(response_to_dict(get_response()))
