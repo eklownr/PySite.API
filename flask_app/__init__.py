c366158375fcd920a2768c6af0324efefaf49962
eklownr <eklownr@gmail.com>
2023-10-31 12:19:25 +0100

    add cors

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:3000"}})
+
+
 from flask_app import routes
